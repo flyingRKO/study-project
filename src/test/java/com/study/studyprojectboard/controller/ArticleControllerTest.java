b801313ea2f022f54349ef59453fbcf50eb589bf
Lee Jae Hun <wogns970@naver.com>
2022-12-05 22:23:48 +0900

    #20 - 게시글 페이지네이션 구현
    '이전글', '다음글'만 구현하므로 게시판에 비해 단순하지만
    첫 글, 마지막 글은 버튼 비활성화 및 가짜 링크 처리해야 해서
    타임리프 문법이 복잡해졌다
    
    마지막 글을 판단하기 위해 총 글 개수가 필요해졌고
    이를 위해 count 쿼리 사용, 서비스 로직 추가

--- src/test/java/com/study/studyprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/study/studyprojectboard/controller/ArticleControllerTest.java
@@ -94,20 +94,24 @@ class ArticleControllerTest {
 
     }
 
-    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @DisplayName("[view][GET] 게시글 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestArticleView_thenReturnsArticleView() throws Exception {
         // Given
         Long articleId = 1L;
+        Long totalCount = 1L;
         given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
+        given(articleService.getArticleCount()).willReturn(totalCount);
         // When & Ten
         mvc.perform(get("/articles/"+articleId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
-                .andExpect(model().attributeExists("articleComments"));
+                .andExpect(model().attributeExists("articleComments"))
+                .andExpect(model().attribute("totalCount", totalCount));
         then(articleService).should().getArticle(articleId);
+        then(articleService).should().getArticleCount();
 
     }
     @Disabled("구현 중")
