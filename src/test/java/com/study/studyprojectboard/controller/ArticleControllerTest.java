45c887d0c798175bb64630789cebec1eec0ca489
Lee Jae Hun <wogns970@naver.com>
2022-12-09 01:32:07 +0900

    #33 - 회원 계정 도메인 변경사항을 영향받는  코드에 적용
    'id' -> 'userId'로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/test/java/com/study/studyprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/study/studyprojectboard/controller/ArticleControllerTest.java
@@ -223,7 +223,6 @@ class ArticleControllerTest {
 
     private UserAccountDto createUserAccountDto() {
         return UserAccountDto.of(
-                1L,
                 "rko",
                 "pw",
                 "rko@mail.com",
